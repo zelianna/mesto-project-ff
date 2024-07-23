783eb80df975b49d082d71440c6ff36c24b0563b
Anna Zelenskaia <anya034@yandex.ru>
2024-07-23 11:08:32 +0200

    Pr5_v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    // Заполняем клонированный элемент данными
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+  
+    deleteButton.addEventListener('click', () => {
+      deleteCard(cardElement);
+    });
+  
+    return cardElement;
+  }
+  
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+  }
+  
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, deleteCard);
+    cardList.append(cardElement);
+  });
+  
